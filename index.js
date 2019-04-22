89be80fcf4abfa687f271bacf9c382146641c3d2
mikeygbd <mikey.soares@gmail.com>
2019-04-22 13:41:46 -0600

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+function addNewElementAsLi() {
+
+  let list = document.querySelector('ul.employee-list');
+  let li = document.createElement("li");
+  li.innerHTML = retrieveEmployeeInformation();
+  list.appendChild(li)
+}
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener("click", function(event) {
+    addNewElementAsLi();
+    document.querySelector('input').value = ''
+  })
+}
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function(event) {
+    document.querySelector('ul.employee-list').innerHTML = "";
+  })
+}
