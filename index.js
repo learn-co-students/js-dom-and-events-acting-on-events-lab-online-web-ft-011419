0be475d17a1aa2c5f486d2045aa5dd178b9e864a
Kang0 <kevinkangg@gmail.com>
2019-04-23 14:44:10 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+    return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  const list = document.querySelector("ul")
+  const newList = document.createElement("li")
+
+  newList.innerHTML = retrieveEmployeeInformation()
+
+  list.appendChild(newList)
+}
+
+function addNewLiOnClick() {
+    const submit = document.querySelector('input[type="submit"]')
+
+    submit.addEventListener("click", function(event){
+      addNewElementAsLi()
+      document.querySelector("input").value = ""
+    })    
+}
+
+function clearEmployeeListOnLinkClick(){
+    const clear = document.querySelector("a")
+    const ul = document.querySelector("ul")
+
+    clear.addEventListener("click", function(event) {
+      ul.innerHTML = ""
+    })
+}
