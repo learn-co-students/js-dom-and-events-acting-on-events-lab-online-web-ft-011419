cb70c37c78547ef6d11fc7846b289e75767f53f5
Robert McCary <robert.mccary@yahoo.com>
2019-04-26 00:52:46 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  const employeeInfo = document.querySelector('input').value
+  return employeeInfo
+}
+
+function addNewElementAsLi(){
+  const unorderedList = document.querySelector("ul")
+  const list = document.createElement("li")
+
+  list.innerHTML = retrieveEmployeeInformation()
+  unorderedList.appendChild(list);
+}
+
+function addNewLiOnClick(){
+  const submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi()
+    document.querySelector('input').value = ""
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  clear = document.querySelector('a')
+  list = document.querySelector('ul')
+  clear.addEventListener("click", function(event){
+    list.innerHTML = ""
+  })
+}
