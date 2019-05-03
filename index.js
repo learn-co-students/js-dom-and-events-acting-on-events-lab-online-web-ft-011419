16871f7c1ffb1dd3f18c7fd6d8d4cd551165044f
gbs4ever <gbs4ever2@gmail.com>
2019-05-03 18:10:09 -0400

    Done.

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ function preventRefreshOnSubmit(){
 
 preventRefreshOnSubmit()
 let input =document.querySelector('input')
+
 function retrieveEmployeeInformation(){
   return input.value
 }
 function addNewElementAsLi(){
-  let ul = document.createElement('ul')
+  let ul = document.querySelector('.employee-list')
+  let employee = retrieveEmployeeInformation()
+
   let li = document.createElement('li')
-  li.innerHTML = retrieveEmployeeInformation()
+  li.innerHTML = employee
   return ul.appendChild(li)
 }
   //input.addEventListener('click', function (event) {
