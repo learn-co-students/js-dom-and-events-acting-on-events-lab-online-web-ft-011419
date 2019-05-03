eb7e3c3248a27e4175b7b5f916de2e4fec03b40c
gbs4ever <gbs4ever2@gmail.com>
2019-05-03 17:26:06 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+let input =document.querySelector('input')
+function retrieveEmployeeInformation(){
+  return input.value
+}
+function addNewElementAsLi(){
+  let ul = document.createElement('ul')
+  let li = document.createElement('li')
+  li.innerHTML = retrieveEmployeeInformation()
+  return ul.appendChild(li)
+}
+  //input.addEventListener('click', function (event) {
+    //console.log(e.which)
+  //})
+//}
